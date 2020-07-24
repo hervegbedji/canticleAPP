2efdb5ebe3e39d116a803a016d129664143f9603
Herve <rvonthecode@gmail.com>
2020-07-24 19:58:46 -0300

    Inserting the lyrics in the canticle array for real. I stopped at canticle 37 for now.

--- src/components/ConsumableDataFromDB.js
+++ src/components/ConsumableDataFromDB.js
@@ -1,6 +1,6 @@
 import canticles_from_db from './DbConnexion.js';
 import CanticleClass from '../classes/canticleClass.js';
-import anotherCanticle from './lyricsT.js'; //actual data
+import anotherCanticle from './lyricsT.js'; //actual data. replace it in DbConnexion when all ready
 
 let displayableCanticleList = anotherCanticle.map((canticleObj) => {
     let arrayOfClassFormatedCanticles = new CanticleClass(canticleObj.theme, canticleObj.number, canticleObj.chorus, canticleObj.verses);
